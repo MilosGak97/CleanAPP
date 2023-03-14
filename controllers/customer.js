c09a97dbef70c62519a94bf6f659214299032a95
Bojan Vreco <milosgak2020@gmail.com>
2023-03-14 12:16:16 -0500

    Dodane rute i controleri

--- controllers/customer.js
+++ controllers/customer.js
@@ -3,3 +3,12 @@ const Move = require('../models/move');
 exports.anyPage = (req,res,next) => {
     res.send('404 Page not found');
 }
+
+exports.getLOGIN = (req,res,next) => {
+    res.render('index');
+}
+
+exports.postLOGIN = (req,res,next) => {
+    console.log(req.body.email);
+    return res.send(req.body.email);
+}
--- routes/customer.js
+++ routes/customer.js
@@ -2,21 +2,10 @@ const express = require('express');
 const customerController = require('../controllers/customer');
 const router = express.Router();
 
+router.get('/login', customerController.getLOGIN);
+
+router.post('/login', customerController.postLOGIN);
+
 router.use('/', customerController.anyPage);
-
-/*
-app.get('/login', (req,res,next) => {
-    res.render('index');
-});
-
-app.post('/login', (req,res,next) => {
-    console.log(req.body.email);
-    return res.send(req.body.email);
-})
-
-app.use('/', (req, res, next) => {
-    res.send('Hello World');
-  });
-  */
 
 module.exports = router;
