1760fc197a1840aee989d43d77ac74d4b32538f7
Bojan Vreco <milosgak2020@gmail.com>
2023-03-14 11:22:46 -0500

    Commit message

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
 
-console.log("Prvi console log");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/login', (req,res,next) => {
+    res.render('index');
+});
+
+app.post('/login', (req,res,next) => {
+    console.log("Console log za login post");
+    return res.send("Uspesno zavrsen kod u login postu");
+})
+
+app.use('/', (req, res, next) => {
+    res.send('Hello World');
+  });
+  
+app.listen(8080);
